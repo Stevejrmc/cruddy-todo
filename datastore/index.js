e8449aabf7c3b9930f07c5b588a9c4fa5e558973
Steve McLaughlin <stevejrmc@gmail.com>
2019-11-12 08:58:06 -0800

    Refactor delete method to use fs to remove todo from dataDir

--- datastore/index.js
+++ datastore/index.js
@@ -60,14 +60,12 @@ exports.update = (id, text, callback) => {
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
+  fs.unlink(exports.dataDir + `/${id}.txt`, err => {
+    if (err) {
+      return callback(err);
+    }
     callback();
-  }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
